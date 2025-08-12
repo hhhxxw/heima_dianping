02ff4c5ea9158fdddcf2e552390ed21e89208635
一只咸鱼的大厂梦 <3025731614@qq.com>
2025-08-12 15:08:48 +0800

    使用乐观锁解决超卖问题

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -33,6 +33,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
 
+
     @Override
     public Result seckillVoucher(Long voucherId) {
         // 1. 查询优惠卷信息
@@ -51,8 +52,13 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
         // 5. 扣减库存
         boolean success = seckillVoucherService.update()
+                // setSql方法可以自定义SQL语句，实现 "SET stock = stock - 1"
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                // eq方法是添加WHERE条件，即 "voucher_id = ?"
+                .eq("voucher_id", voucherId)
+                // gt方法也是添加WHERE条件，即 "AND stock > ?"
+                .gt("stock", 0)
+                .update();
         if(!success){
             // 扣减失败
             return Result.fail("库存不足");
